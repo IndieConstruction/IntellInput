0421ec4ff5b56558750eb78b4985050f867dc07a
gabrieleancona <gabry.ancona@gmail.com>
2019-06-18 16:04:17 +0200

    Aggiunto tutti eventi rimanenti(trigger,pause,guide,start,Dpad,LB,RB)

--- Assets/Scripts/Common/IntellGamePad.cs
+++ Assets/Scripts/Common/IntellGamePad.cs
@@ -13,7 +13,9 @@ public class IntellGamePad {
     public Action<IntellGamePad, Buttons> OnButtonReleased;
     public Action<IntellGamePad, Buttons, AxisValue> OnAxisUsed;
     public Action<IntellGamePad, Buttons, AxisValue> OnAxisStopUsed;
-    #endregion
+    public Action<IntellGamePad, Buttons, float> OnTriggerUsed;
+    public Action<IntellGamePad, Buttons, float> OnTriggerStopUsed;
+    #endregion  
 
     #region Struct
     [Serializable]
@@ -22,10 +24,16 @@ public class IntellGamePad {
         public float rightStickDeadzone;
         [Range(0, 1)]
         public float leftStickDeadzone;
+        [Range(0, 1)]
+        public float RTDeadzone;
+        [Range(0, 1)]
+        public float LTDeadzone;
 
-        public Settings(float _rightStickDeadzone, float _leftStickDeadzone) {
+        public Settings(float _rightStickDeadzone, float _leftStickDeadzone , float _RTDeadzone = 0, float _LTDeadzone = 0) {
             rightStickDeadzone = Mathf.Abs(_rightStickDeadzone);
             leftStickDeadzone = Mathf.Abs(_leftStickDeadzone);
+            RTDeadzone = Mathf.Abs(_RTDeadzone);
+            LTDeadzone = Mathf.Abs(_LTDeadzone);
         }
     }
 
@@ -85,21 +93,32 @@ public class IntellGamePad {
         ID = id;
     }
 
+    #region Buttons Events
     private void CheckButtons() {
         //X Button
-        if (OldGamePadState.Buttons.X == ButtonState.Released && CurrentGamePadState.Buttons.X == ButtonState.Pressed) {
+        if (OldGamePadState.Buttons.X == ButtonState.Released && CurrentGamePadState.Buttons.X == ButtonState.Pressed)
+        {
             OnButtonPressed?.Invoke(this, Buttons.X);
-        } else if (OldGamePadState.Buttons.X == ButtonState.Pressed && CurrentGamePadState.Buttons.X == ButtonState.Pressed) {
+        }
+        else if (OldGamePadState.Buttons.X == ButtonState.Pressed && CurrentGamePadState.Buttons.X == ButtonState.Pressed)
+        {
             OnButtonHold?.Invoke(this, Buttons.X);
-        } else if (OldGamePadState.Buttons.X == ButtonState.Pressed && CurrentGamePadState.Buttons.X == ButtonState.Released) {
+        }
+        else if (OldGamePadState.Buttons.X == ButtonState.Pressed && CurrentGamePadState.Buttons.X == ButtonState.Released)
+        {
             OnButtonReleased?.Invoke(this, Buttons.X);
         }
         //A Button
-        if (OldGamePadState.Buttons.A == ButtonState.Released && CurrentGamePadState.Buttons.A == ButtonState.Pressed) {
+        if (OldGamePadState.Buttons.A == ButtonState.Released && CurrentGamePadState.Buttons.A == ButtonState.Pressed)
+        {
             OnButtonPressed?.Invoke(this, Buttons.A);
-        } else if (OldGamePadState.Buttons.A == ButtonState.Pressed && CurrentGamePadState.Buttons.A == ButtonState.Pressed) {
+        }
+        else if (OldGamePadState.Buttons.A == ButtonState.Pressed && CurrentGamePadState.Buttons.A == ButtonState.Pressed)
+        {
             OnButtonHold?.Invoke(this, Buttons.A);
-        } else if (OldGamePadState.Buttons.A == ButtonState.Pressed && CurrentGamePadState.Buttons.A == ButtonState.Released) {
+        }
+        else if (OldGamePadState.Buttons.A == ButtonState.Pressed && CurrentGamePadState.Buttons.A == ButtonState.Released)
+        {
             OnButtonReleased?.Invoke(this, Buttons.A);
         }
         //B Button
@@ -128,6 +147,150 @@ public class IntellGamePad {
         {
             OnButtonReleased?.Invoke(this, Buttons.Y);
         }
+        //Start Button
+        if (OldGamePadState.Buttons.Start == ButtonState.Released && CurrentGamePadState.Buttons.Start == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.Start);
+        }
+        else if (OldGamePadState.Buttons.Start == ButtonState.Pressed && CurrentGamePadState.Buttons.Start == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.Start);
+        }
+        else if (OldGamePadState.Buttons.Start == ButtonState.Pressed && CurrentGamePadState.Buttons.Start == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.Start);
+        }
+        //Pause Button
+        if (OldGamePadState.Buttons.Back == ButtonState.Released && CurrentGamePadState.Buttons.Back == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.Pause);
+        }
+        else if (OldGamePadState.Buttons.Back == ButtonState.Pressed && CurrentGamePadState.Buttons.Back == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.Pause);
+        }
+        else if (OldGamePadState.Buttons.Back == ButtonState.Pressed && CurrentGamePadState.Buttons.Back == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.Pause);
+        }
+        //RB Button
+        if (OldGamePadState.Buttons.RightShoulder == ButtonState.Released && CurrentGamePadState.Buttons.RightShoulder == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.RB);
+        }
+        else if (OldGamePadState.Buttons.RightShoulder == ButtonState.Pressed && CurrentGamePadState.Buttons.RightShoulder == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.RB);
+        }
+        else if (OldGamePadState.Buttons.RightShoulder == ButtonState.Pressed && CurrentGamePadState.Buttons.RightShoulder == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.RB);
+        }
+        //LB Button
+        if (OldGamePadState.Buttons.LeftShoulder == ButtonState.Released && CurrentGamePadState.Buttons.LeftShoulder == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.LB);
+        }
+        else if (OldGamePadState.Buttons.LeftShoulder == ButtonState.Pressed && CurrentGamePadState.Buttons.LeftShoulder == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.LB);
+        }
+        else if (OldGamePadState.Buttons.LeftShoulder == ButtonState.Pressed && CurrentGamePadState.Buttons.LeftShoulder == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.LB);
+        }
+        //Guide Button
+        if (OldGamePadState.Buttons.Guide == ButtonState.Released && CurrentGamePadState.Buttons.Guide == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.Guide);
+        }
+        else if (OldGamePadState.Buttons.Guide == ButtonState.Pressed && CurrentGamePadState.Buttons.Guide == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.Guide);
+        }
+        else if (OldGamePadState.Buttons.Guide == ButtonState.Pressed && CurrentGamePadState.Buttons.Guide == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.Guide);
+        }
+        //LeftStick Button
+        if (OldGamePadState.Buttons.LeftStick == ButtonState.Released && CurrentGamePadState.Buttons.LeftStick == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.LeftStick);
+        }
+        else if (OldGamePadState.Buttons.LeftStick == ButtonState.Pressed && CurrentGamePadState.Buttons.LeftStick == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.LeftStick);
+        }
+        else if (OldGamePadState.Buttons.LeftStick == ButtonState.Pressed && CurrentGamePadState.Buttons.LeftStick == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.LeftStick);
+        }
+        //RightStick Button
+        if (OldGamePadState.Buttons.RightStick == ButtonState.Released && CurrentGamePadState.Buttons.RightStick == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.RightStick);
+        }
+        else if (OldGamePadState.Buttons.RightStick == ButtonState.Pressed && CurrentGamePadState.Buttons.RightStick == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.RightStick);
+        }
+        else if (OldGamePadState.Buttons.RightStick == ButtonState.Pressed && CurrentGamePadState.Buttons.RightStick == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.RightStick);
+        }
+        //DPadLeft Button
+        if (OldGamePadState.DPad.Left == ButtonState.Released && CurrentGamePadState.DPad.Left == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.DPadLeft);
+        }
+        else if (OldGamePadState.DPad.Left == ButtonState.Pressed && CurrentGamePadState.DPad.Left == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.DPadLeft);
+        }
+        else if (OldGamePadState.DPad.Left == ButtonState.Pressed && CurrentGamePadState.DPad.Left == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.DPadLeft);
+        }
+        //DPadRight Button
+        if (OldGamePadState.DPad.Right == ButtonState.Released && CurrentGamePadState.DPad.Right == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.DPadRight);
+        }
+        else if (OldGamePadState.DPad.Right == ButtonState.Pressed && CurrentGamePadState.DPad.Right == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.DPadRight);
+        }
+        else if (OldGamePadState.DPad.Right == ButtonState.Pressed && CurrentGamePadState.DPad.Right == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.DPadRight);
+        }
+        //DPadUp Button
+        if (OldGamePadState.DPad.Up == ButtonState.Released && CurrentGamePadState.DPad.Up == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.DPadUp);
+        }
+        else if (OldGamePadState.DPad.Up == ButtonState.Pressed && CurrentGamePadState.DPad.Up == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.DPadUp);
+        }
+        else if (OldGamePadState.DPad.Up == ButtonState.Pressed && CurrentGamePadState.DPad.Up == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.DPadUp);
+        }
+        //DPadDown Button
+        if (OldGamePadState.DPad.Down == ButtonState.Released && CurrentGamePadState.DPad.Down == ButtonState.Pressed)
+        {
+            OnButtonPressed?.Invoke(this, Buttons.DPadDown);
+        }
+        else if (OldGamePadState.DPad.Down == ButtonState.Pressed && CurrentGamePadState.DPad.Down == ButtonState.Pressed)
+        {
+            OnButtonHold?.Invoke(this, Buttons.DPadDown);
+        }
+        else if (OldGamePadState.DPad.Down == ButtonState.Pressed && CurrentGamePadState.DPad.Down == ButtonState.Released)
+        {
+            OnButtonReleased?.Invoke(this, Buttons.DPadDown);
+        }
+        #endregion
 
         //Left Axis
         float lsX = CurrentGamePadState.ThumbSticks.Left.X;
@@ -139,15 +302,26 @@ public class IntellGamePad {
         float rsY = CurrentGamePadState.ThumbSticks.Right.Y;
         float oldrsX = OldGamePadState.ThumbSticks.Right.X;
         float oldrsY = OldGamePadState.ThumbSticks.Right.Y;
+        //RT Axis
+        float rt = CurrentGamePadState.Triggers.Right;
+        float oldrt = OldGamePadState.Triggers.Right;
+        //LT Axis
+        float lt = CurrentGamePadState.Triggers.Left;
+        float oldlt = OldGamePadState.Triggers.Left;
+
+
 
         //Left axis
         if ((lsX > currentSettings.leftStickDeadzone || lsX < -currentSettings.leftStickDeadzone) ||
-            (lsY > currentSettings.leftStickDeadzone || lsY < -currentSettings.leftStickDeadzone)) {
+            (lsY > currentSettings.leftStickDeadzone || lsY < -currentSettings.leftStickDeadzone))
+        {
             OnAxisUsed?.Invoke(this, Buttons.LeftStick, new AxisValue(lsX, lsY));
-        } else if ((currentSettings.leftStickDeadzone > lsX && lsX < -currentSettings.leftStickDeadzone) &&
+        }
+        else if ((currentSettings.leftStickDeadzone > lsX && lsX < -currentSettings.leftStickDeadzone) &&
                    (currentSettings.leftStickDeadzone > lsY && lsY < -currentSettings.leftStickDeadzone) &&
                    (oldlsX > currentSettings.leftStickDeadzone || oldlsX < -currentSettings.leftStickDeadzone) ||
-                   (oldlsY > currentSettings.leftStickDeadzone || oldlsY < -currentSettings.leftStickDeadzone)) {
+                   (oldlsY > currentSettings.leftStickDeadzone || oldlsY < -currentSettings.leftStickDeadzone))
+        {
             OnAxisStopUsed?.Invoke(this, Buttons.LeftStick, new AxisValue(lsX, lsY));
         }
 
@@ -164,6 +338,26 @@ public class IntellGamePad {
         {
             OnAxisStopUsed?.Invoke(this, Buttons.RightStick, new AxisValue(rsX, rsY));
         }
+
+        //RT axis
+        if (rt > currentSettings.RTDeadzone)
+        {
+            OnTriggerUsed?.Invoke(this, Buttons.RT , rt);
+        }
+        else if (currentSettings.RTDeadzone > rt && oldrt > currentSettings.RTDeadzone)
+        {
+            OnTriggerStopUsed?.Invoke(this, Buttons.RT , rt);
+        }
+        //LT axis
+        if (lt > currentSettings.LTDeadzone)
+        {
+            OnTriggerUsed?.Invoke(this, Buttons.LT, lt);
+        }
+        else if (currentSettings.LTDeadzone > lt && oldlt > currentSettings.LTDeadzone)
+        {
+            OnTriggerStopUsed?.Invoke(this, Buttons.LT, lt);
+        }
+
     }
 
     public void SetSettings(Settings _settings) {
